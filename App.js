fa1c50031f08779e98323e6ec0ccef469328e9d3
Jhoncar7 <jhon7car@gmail.com>
2021-11-25 14:57:51 -0300

    avances

--- App.js
+++ App.js
@@ -6,7 +6,11 @@ import { vibrate } from './utils';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button title={'Vibracion Phone'} onPress={()=>{vibrate()}}/>
+      {/* <Button title={'Vibracion Phone'} onPress={()=>{vibrate()}}/> */}
+      <Text style={[styles.title, styles.center]}>Cronometro Pomodoro!</Text>
+      <View>
+        <Text style={[styles.title, styles.center]}>00:00</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,9 +18,19 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //una sola columna
+    backgroundColor: '#fff', //color de fondo blanco
+    //alignItems: 'center',//centra los elementos
+    alignItems:'stretch',
+    //justifyContent: 'center',// de arriba abajo
+    paddingTop: 150,
   },
+  title:{
+    fontSize: 35,
+    fontWeight: 'bold',
+    textTransform:'capitalize'
+  },
+  center:{
+    alignSelf:'center',
+  }
 });
